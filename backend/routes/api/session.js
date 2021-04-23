1625779df21d90884655617a3add432f51b71ac8
Ondre <ondre.williams.77@gmail.com>
2021-04-23 13:07:27 -0700

    validation done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,27 @@
 // backend/routes/api/session.js
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -28,6 +41,20 @@ router.post(
       user,
     });
   }),
+);
+
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
 );
 
 // Log out
